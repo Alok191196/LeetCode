8c15ad141fef786716a3947ce9a15c9443f7a099
Alok kumar Singh <alok@market-pulse.in>
2024-01-11 00:40:15 +0530

    Added merge sort array

---
+++ src/main/kotlin/MergeSortArray.kt
@@ -0,0 +1,6 @@
+class MergeSortArray {
+    fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int): Unit {
+        var num1List = nums1.asList().toMutableList()
+        var num2List = nums2.asList().toMutableList()
+    }
+}
