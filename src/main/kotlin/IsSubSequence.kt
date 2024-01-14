560785c3eeab315a761d6bf36011c80950b0957e
Alok kumar Singh <aloksingh191196@gmail.com>
2024-01-14 13:01:18 +0530

    Added IsSubSequence Soultion

---
+++ src/main/kotlin/IsSubSequence.kt
@@ -0,0 +1,20 @@
+class IsSubSequence {
+    fun find(s: String, t: String): Boolean {
+        var i = 0
+        var j = 0
+        while (i < s.length && j < t.length) {
+            if (s[i] == t[j]) {
+                i += 1
+                j += 1
+            } else {
+                j += 1
+            }
+        }
+        return i == s.length
+    }
+}
+
+fun main() {
+    println(IsSubSequence().find("ace", "abcde"))
+    println(IsSubSequence().find("aaaaaa", "bbaaaa"))
+}
