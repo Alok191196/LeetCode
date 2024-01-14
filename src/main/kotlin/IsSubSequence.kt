2e15657cd7566e9a5d70f9e24948e36da94542b9
Alok kumar Singh <aloksingh191196@gmail.com>
2024-01-14 13:07:48 +0530

    optimised isSubSequence problem

--- src/main/kotlin/IsSubSequence.kt
+++ src/main/kotlin/IsSubSequence.kt
@@ -1,5 +1,6 @@
 class IsSubSequence {
     fun find(s: String, t: String): Boolean {
+        if (s.isEmpty()) return true
         var i = 0
         var j = 0
         while (i < s.length && j < t.length) {
