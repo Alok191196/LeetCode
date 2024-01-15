74c12b5b47b481fbc3125986bc3aad546b0dabe5
Alok kumar Singh <aloksingh191196@gmail.com>
2024-01-15 22:24:43 +0530

    added subarray code

---
+++ src/main/kotlin/SubArray.kt
@@ -0,0 +1,13 @@
+class SubArray {
+    fun printSubArray(intArrayOf: IntArray) {
+        var p1 = 0
+        var p2 = intArrayOf.size - 1
+        for (i in intArrayOf.indices) {
+
+        }
+    }
+}
+
+fun main() {
+    SubArray().printSubArray(intArrayOf(1, 2, 3, 4, 5, 6))
+}
