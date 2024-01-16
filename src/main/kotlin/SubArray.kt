f15a82705d63335a942ce4a7dd11104aa18fe092
Alok kumar Singh <aloksingh191196@gmail.com>
2024-01-16 23:46:42 +0530

    added sudo code

--- src/main/kotlin/SubArray.kt
+++ src/main/kotlin/SubArray.kt
@@ -1,10 +1,21 @@
+// Sudo code
+//left = 0
+//curr = 0
+//answer = 0
+//for (int right = 0; right < nums.length; right++):
+//curr += nums[right]
+//while (curr > k):
+//curr -= nums[left]
+//left++
+//
+//answer = max(answer, right - left + 1)
+
 class SubArray {
     fun printSubArray(intArrayOf: IntArray) {
         var p1 = 0
         var p2 = intArrayOf.size - 1
-        for (i in intArrayOf.indices) {
 
-        }
+
     }
 }
 
